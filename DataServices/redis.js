e68ef92f0465ee16687eefe16554a97ec05d88e9
stevan <stevan.thomas@prodigysoft.net>
2018-09-17 18:08:09 -0400

    updated the warehouse sectioin

--- DataServices/redis.js
+++ DataServices/redis.js
@@ -25,13 +25,51 @@ var set = (key,value)=> {
 var hashset = (key,value)=>{
     return new Promise((resolve,reject)=>{
         if (key == null) reject(); 
-        client.hmset('packages:'+key,value,(error,data)=>{
+        client.hmset(key,value,(error,data)=>{
             if (error) 
                 reject(error); 
             resolve(data); 
          });
     });
   
+}
+var getPackage = (key)=> {
+    return new Promise((resolve,reject)=>{
+        console.log('key = packages:' +key); 
+        if (key == null) reject(); 
+        client.hgetall('packages:'+key,(error,data)=>{
+              
+                if (error ){
+                    reject(error); 
+                    console.log(error);
+                } 
+                console.log(data);
+                resolve(data)
+        }); 
+    });
+}
+var setAdd  = (key,value)=>{
+    return new Promise((resolve,reject)=>{
+        if (key == null) reject(); 
+        client.sadd(key,value,(error,data)=>{
+            
+                if (error ) reject(error); 
+                resolve(data)
+        }); 
+    });
+}
+var getMembers = (key)=>{
+    return new Promise((resolve,reject)=>{
+        if (key == null) reject(); 
+        client.smembers(key,(error,data)=>{
+                if (error ) {reject(error);
+               
+                } 
+                console.log('printing smembers')
+                console.log(data);
+                resolve(data)
+        }); 
+    });
 }
 var hmgetall = (key)=> {
     return new Promise((resolve,reject)=>{
@@ -43,11 +81,33 @@ var hmgetall = (key)=> {
     });
 }
 
-
+var getNSRecords =  (key)=>{
+    return new Promise((resolve,reject)=>{
+        if (key == null) reject(); 
+        client.scan('0','MATCH',key,(error,data)=>{
+                if (error ) reject(error); 
+                resolve(data)
+        }); 
+    });
+}
+var delkey = (key)=>{
+    return new Promise((resolve,reject)=>{
+        if (key == null) reject(); 
+        client.del(key,(error,data)=>{
+                if (error ) reject(error); 
+                resolve(data)
+        }); 
+    });
+}
 
 
 module.exports.set = set;
 module.exports.get = get; 
+module.exports.getPackage = getPackage;
+module.exports.getNS  = getNSRecords;
+module.exports.setAdd = setAdd; 
+module.exports.del = delkey; 
 module.exports.hmset = hashset; 
+module.exports.getMembers = getMembers; 
 module.exports.hgetall = hmgetall; 
 module.exports.client = client; 
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -26,6 +26,7 @@ router.get('/m-packages/:manifestId', middleware(services.userService).requireAu
     pageData.RoleId = res.User.RoleId;
     pageData.mid = Number(req.params.manifestId);
     console.log(pageData.mid);
+    
     //we want to format the manifest number to 3 digits 
     res.render('pages/warehouse/manifest-packages', pageData);
 });
@@ -64,6 +65,62 @@ router.post('/get-customer-info', middleware(services.userService).requireAuthen
     });
     
 });
+router.get('/get-mpackages/:mid',middleware(services.userService).requireAuthentication, (req, res, next) => {
+    var manifestKey = `manifest:${package.mid}:${package.mtype}:${container}:${containerNo}`; 
+    redis.getMembers(manifestKey).then(function(members){
+        res.send(members)
+    }); 
+});
+router.post('/save-package',middleware(services.userService).requireAuthentication, (req, res, next) => {
+    var body = req.body;
+    var package = {
+        trackingNo: body.tracking,
+        description: body.description,
+        shipper: body.shipper,
+        value: Number(body.value),
+        pieces: Number(body.pieces),
+        weight: Number(body.weight),
+        mid:body.mid,
+        mtype:body.mtype
+    }
+    var container = ""; 
+    var containerNo = ""
+    if (typeof body.bag != "undefined"){
+        package.bag = body.bag;
+        container= "bag";
+        containerNo = package.bag; 
+    }
+    if(typeof body.skid != "undefined"){
+        package.skid = body.skid; 
+        container = "skid"; 
+        containerNo = package.skid; 
+    }
+    //save the package to the package NS
+    redis.hmset('packages:'+package.trackingNo,package).then(function(result){
+        var manifestKey = `manifest:${package.mid}:${package.mtype}:${container}:${containerNo}`; 
+        redis.setAdd(manifestKey,package.trackingNo).then(function(sResult){
+            //get the members one time here 
+            console.log(manifestKey); 
+            redis.getMembers(manifestKey)
+            .then( (data) => Promise.all(data.map(redis.getPackage)))
+            .then( function(data){
+                res.send({saved:true, packages:data});
+            }).catch((err3)=>{
+                res.send({err:err3,saved:true,listing:false})
+            });
+            
+        }).catch(function(err){
+            res.send({saved:false});
+        }); 
+    }).catch(function(err2){
+        res.send({saved:false})
+    }); 
+    
+    
+
+
+    console.log(package);
+});
 router.post('/packages', middleware(services.userService).requireAuthentication, (req, res, next) => {
     var body = req.body;
     var package = {
@@ -74,8 +131,8 @@ router.post('/packages', middleware(services.userService).requireAuthentication,
         pieces: Number(body.pieces),
         weight: Number(body.weight)
     
-    }
-    redis.hmset(package.trackingNo,package); 
+    };
+    redis.hmset('packages:'+package.trackingNo,package); 
     console.log(package);
     res.redirect('/warehouse/packages'); 
 });
