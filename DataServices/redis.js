2477545d99586a58d2be9ffdb2c637212cebc0d1
Stevan Thomas <stevan@vela.global>
2018-09-17 06:14:35 -0400

    updated the packages

--- DataServices/redis.js
+++ DataServices/redis.js
@@ -1,8 +1,8 @@
 var redis  = require('redis'); 
 const NSPACE_CUST = "skybox:"; 
-const NSPACE_BOX = "customer:"; 
+const NSPACE_BOX = "tew:owner:"; 
 
-var client = redis.createClient(6380,"hikett-test.redis.cache.windows.net",{auth_pass: 'MgchpaUTzMSo5f4SAQsXwjau6r2OdY6OLHOV2sLQYKg=', tls: {servername: 'hikett-test.redis.cache.windows.net'}});
+var client = redis.createClient(6380,"tew-redis.redis.cache.windows.net",{auth_pass: 'euAj2NlScSkbO52pTt0LoEX7RajlFnZlmJ+baDt05Cw=', tls: {servername: 'tew-redis.redis.cache.windows.net'}});
 
 var get = (key)=> {
     return new Promise((resolve,reject)=>{
@@ -22,65 +22,32 @@ var set = (key,value)=> {
         }); 
     });
 }
-
-var getLoc = (key)=> {
+var hashset = (key,value)=>{
     return new Promise((resolve,reject)=>{
         if (key == null) reject(); 
-        client.get(`driver:location:${key}`,(error,data)=>{
-                if (error ) reject(error); 
-                var driverLoc = { 
-                    username:key, 
-                    location:data
-                };
-                resolve(driverLoc)
-        }); 
+        client.hmset('packages:'+key,value,(error,data)=>{
+            if (error) 
+                reject(error); 
+            resolve(data); 
+         });
     });
+  
 }
-var scanLocations = (match)=>{
-    return new Promise((resolve,reject)=>{
-        client.scan('0','MATCH','system:driver:*', (error,data)=>{
-            console.log('output from scand'); 
-            //console.log(data); 
-            if (data)
-            {
-                var keys = data[1]; 
-                var usernames = [];
-                for(i=0;i<keys.length;i++){
-                    //get the usernames only 
-                    var driver = keys[i].replace("system:driver:",""); 
-                    usernames.push(driver); 
-                }
-                resolve(usernames); 
-            }
-             
-        }); 
-    });
-}
-var smembers = (key)=>{
+var hmgetall = (key)=> {
     return new Promise((resolve,reject)=>{
         if (key == null) reject(); 
-        client.smembers(key,(error,data)=>{
+        client.hgetall(key,(error,data)=>{
                 if (error ) reject(error); 
-                console.log('printing smembers')
                 resolve(data)
         }); 
     });
 }
-var details = (data)=>
-{
-    return new Promise((resolve,reject)=>{
-        console.log(data); 
-       
-        client.hgetall("driver:details:"+data.username,(error,rtnData)=>{
-            if (error ) reject(error);
-                data.details = rtnData; 
-                resolve(data); 
-        }); 
-    });  
-}
-module.exports.scanLoc = scanLocations; 
+
+
+
+
+module.exports.set = set;
 module.exports.get = get; 
-module.exports.smembers = smembers;
-module.exports.driverLocation = getLoc;  
-module.exports.driverDetails = details; 
+module.exports.hmset = hashset; 
+module.exports.hgetall = hmgetall; 
 module.exports.client = client; 
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,49 +1,82 @@
 var express = require('express');
 var router = express.Router();
-var services = require('../DataServices/services'); 
-var middleware = require('../middleware'); 
-var moment = require('moment'); 
+var services = require('../DataServices/services');
+var middleware = require('../middleware');
+var moment = require('moment');
+var redis = require('../DataServices/redis'); 
 //Manifest Routes
-router.get('/list-manifest',middleware(services.userService).requireAuthentication,(req,res,next)=>{
-    var pageData = {}; 
-    pageData.title = "Manifest"; 
-    pageData.luser = res.User.FirstName+ ' '+res.User.LastName;
-    pageData.RoleId = res.User.RoleId; 
-    services.manifestService.listAllManifest().then((result)=>{
-        console.log('listing'); 
+router.get('/list-manifest', middleware(services.userService).requireAuthentication, (req, res, next) => {
+    var pageData = {};
+    pageData.title = "Manifest";
+    pageData.luser = res.User.FirstName + ' ' + res.User.LastName;
+    pageData.RoleId = res.User.RoleId;
+    services.manifestService.listAllManifest().then((result) => {
+        console.log('listing');
         console.log(result);
-        pageData.listing = result.listing; 
+        pageData.listing = result.listing;
 
-        res.render('pages/warehouse/list-manifest',pageData); 
+        res.render('pages/warehouse/list-manifest', pageData);
+    });
+
+});
+router.get('/m-packages/:manifestId', middleware(services.userService).requireAuthentication, function (req, res, next) {
+    var pageData = {};
+    pageData.title = "Manifest Packages";
+    pageData.luser = res.User.FirstName + ' ' + res.User.LastName;
+    pageData.RoleId = res.User.RoleId;
+    pageData.mid = Number(req.params.manifestId);
+    console.log(pageData.mid);
+    //we want to format the manifest number to 3 digits 
+    res.render('pages/warehouse/manifest-packages', pageData);
+});
+router.post('/create-manifest', middleware(services.userService).requireAuthentication, function (req, res, next) {
+    console.log(res.User);
+    services.manifestService.createManfiest(res.User.Username).then((result) => {
+        res.send(result);
+    });
+});
+router.get('/mlist', middleware(services.userService).requireAuthentication, (req, res, next) => {
+    services.manifestService.listAllManifest().then((result) => {
+        res.send(result.listing);
+    });
+});
+router.get('/packages', middleware(services.userService).requireAuthentication, (req, res, next) => {
+    var pageData = {};
+    pageData.title = "Add Packages";
+    pageData.luser = res.User.FirstName + ' ' + res.User.LastName;
+    pageData.RoleId = res.User.RoleId;
+    res.render('pages/warehouse/add-package.ejs', pageData);
+
+    
+});
+router.post('/get-customer-info', middleware(services.userService).requireAuthentication, (req, res, next) => {
+    //get customer information 
+    var skybox = req.body.box; 
+    redis.hgetall('tew:owners:'+skybox).then((result)=>{
+        console.log(result);
+        if (result == null )
+            res.send({err:'Could not get customer information'});
+        //send err
+        else  
+            res.send(result);
+    }).catch((err)=>{
+        res.send({err:'Could not get customer information'});
     });
     
-}); 
-router.get('/m-packages/:manifestId',middleware(services.userService).requireAuthentication,function(req,res,next){
-    var pageData = {}; 
-    pageData.title = "Manifest Packages"; 
-    pageData.luser = res.User.FirstName+ ' '+res.User.LastName;
-    pageData.RoleId = res.User.RoleId; 
-    res.render('pages/warehouse/manifest-packages',pageData);
 });
-router.post('/create-manifest',middleware(services.userService).requireAuthentication,function(req,res,next){
-    console.log(res.User);
-    services.manifestService.createManfiest(res.User.Username).then((result)=>{
-        res.send(result); 
-    });
-});
-router.get('/mlist',middleware(services.userService).requireAuthentication,(req,res,next)=>{
-    services.manifestService.listAllManifest().then((result)=>{
-       res.send(result.listing);
-    });
-});
-router.get('/packages',middleware(services.userService).requireAuthentication,(req,res,next)=>{
-    var pageData = {}; 
-    pageData.title = "Add Packages"; 
-    pageData.luser = res.User.FirstName+ ' '+res.User.LastName;
-    pageData.RoleId = res.User.RoleId; 
-    res.render('pages/warehouse/add-package.ejs',pageData);
-});
-router.post('/packages',middleware(services.userService).requireAuthentication,(req,res,next)=>{
-
+router.post('/packages', middleware(services.userService).requireAuthentication, (req, res, next) => {
+    var body = req.body;
+    var package = {
+        trackingNo: body.trackingNo,
+        description: body.description,
+        shipper: body.shipper,
+        value: Number(body.value),
+        pieces: Number(body.pieces),
+        weight: Number(body.weight)
+    
+    }
+    redis.hmset(package.trackingNo,package); 
+    console.log(package);
+    res.redirect('/warehouse/packages'); 
 });
 module.exports = router;
