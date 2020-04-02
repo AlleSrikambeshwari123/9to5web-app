340c04fd34679513a272cfdc11fb323ac6f23305
Eden2016 <eden201621@gmail.com>
2020-04-02 06:53:54 -0700

    fix generation of package labels

--- Controller/AwbController.js
+++ Controller/AwbController.js
@@ -1,7 +1,7 @@
-var services = require('../RedisServices/RedisDataServices');
-var printerCtrl = require('./PrinterController');
-var utils = require('../Util/utils');
-var aws = require('../Util/aws');
+const services = require('../RedisServices/RedisDataServices');
+const printerCtrl = require('./PrinterController');
+const utils = require('../Util/utils');
+const aws = require('../Util/aws');
 
 exports.preview_awb = (req, res, next) => {
   let id = req.params.id;
@@ -38,10 +38,10 @@ exports.preview_awb = (req, res, next) => {
       })
     })
   })
-}
+};
 
 exports.get_awb_detail = (req, res, next) => {
-  var id = req.params.id;
+  const id = req.params.id;
   Promise.all([
     services.customerService.getCustomers(),
     services.hazmatService.getHazmats(),
@@ -54,6 +54,7 @@ exports.get_awb_detail = (req, res, next) => {
     services.paidTypeService.getAllPaidTypes(),
     services.serviceTypeService.getAllServiceTypes(),
     services.awbService.getPurchaseOrder(id),
+    services.packageService.getAllOriginBarcode(),
   ]).then(([
     customers,
     hazmats,
@@ -66,8 +67,8 @@ exports.get_awb_detail = (req, res, next) => {
     paidTypes,
     serviceTypes,
     purchaseOrder,
+    barcodes,
   ]) => {
-    //console.log(results[4]);
     res.render('pages/warehouse/awb/edit', {
       page: req.originalUrl,
       title: 'AWB Details',
@@ -84,9 +85,10 @@ exports.get_awb_detail = (req, res, next) => {
       paidTypes,
       serviceTypes,
       purchaseOrder,
+      barcodes,
     });
   })
-}
+};
 
 exports.create_awb = (req, res, next) => {
   Promise.all([
@@ -123,10 +125,10 @@ exports.create_awb = (req, res, next) => {
       barcodes
     });
   })
-}
+};
 
 exports.add_new_awb = (req, res, next) => {
-  let { invoices, ...awb } = req.body;
+  let {invoices, ...awb} = req.body;
   let packages = JSON.parse(awb.packages);
   services.awbService.createAwb(awb).then(async result => {
     awb = result.awb;
@@ -139,27 +141,23 @@ exports.add_new_awb = (req, res, next) => {
       pkg.shipperId = awb.shipper;
       pkg.carrierId = awb.carrier;
       pkg.hazmatId = awb.hazmat;
-    })
-    services.packageService.createPackages(awb.id, packages).then(packageResult => {
-      res.send(result);
-    })
-  })
-}
+    });
+    services.packageService.createPackages(awb.id, packages)
+      .then(() => res.send(result));
+  });
+};
 
 exports.update_awb = (req, res, next) => {
   let awb_id = parseInt(req.params.id);
-  let { invoices, ...awb } = req.body;
+  let {invoices, ...awb} = req.body;
 
   let packages = JSON.parse(awb.packages);
-  services.awbService.updateAwb(awb_id,awb).then(async result => {
-    //awb = result.awb;
+  services.awbService.updateAwb(awb_id, awb).then(async result => {
     let all = (invoices || []).map(async (invoice) => {
       invoice.awbId = awb_id;
       if (invoice.id) {
-        console.log('UPDATE incoive', invoice)
         await services.invoiceService.update(invoice.id, invoice);
       } else {
-        console.log('CREATE incoive', invoice)
         await services.invoiceService.create(invoice);
       }
     });
@@ -169,25 +167,25 @@ exports.update_awb = (req, res, next) => {
       pkg.shipperId = awb.shipper;
       pkg.carrierId = awb.carrier;
       pkg.hazmatId = awb.hazmat;
-          //Old package need in updates
-      if(pkg.awbId!==undefined){
+      //Old package need in updates
+      if (pkg.awbId !== undefined) {
         all.push(services.packageService.updatePackage(pkg.id, pkg))
       }
     });
 
-    const new_packages = packages.filter(f=>f.awbId===undefined);
-      //Adding new packages
+    const new_packages = packages.filter(f => f.awbId === undefined);
+    //Adding new packages
     all.push(
-        services.packageService.createPackages(awb_id, new_packages)
+      services.packageService.createPackages(awb_id, new_packages)
     );
 
     Promise.all(all).then(packageResult => {
       res.send(result);
     })
-  }).catch(err=>{
+  }).catch(err => {
     console.log(err);
   })
-}
+};
 
 exports.get_awb_list = (req, res, next) => {
   services.awbService.getAwbs().then(awbs => {
@@ -200,12 +198,11 @@ exports.get_awb_list = (req, res, next) => {
       })
     })
   })
-}
+};
 
 exports.get_awb_no_docs = (req, res, next) => {
   services.awbService.getAwbsNoDocs().then(awbs => {
     getFullAwb(awbs).then(awbs => {
-      console.log(awbs)
       res.render('pages/warehouse/awb/no-docs', {
         page: req.originalUrl,
         title: "AirWay Bills - No Docs",
@@ -214,7 +211,7 @@ exports.get_awb_no_docs = (req, res, next) => {
       })
     })
   })
-}
+};
 
 exports.delete_awb = (req, res, next) => {
   let awbId = req.params.id;
@@ -224,13 +221,13 @@ exports.delete_awb = (req, res, next) => {
   ]).then(results => {
     res.send(results[0]);
   })
-}
+};
 
 exports.generate_awb_pdf = (req, res, next) => {
   printerCtrl.generate_awb_pdf(req.params.awbId).then(result => {
     res.send(result);
   })
-}
+};
 
 exports.nas_no_docs = (req, res, next) => {
   services.awbService.getAwbsNoDocs().then(awbs => {
@@ -243,7 +240,7 @@ exports.nas_no_docs = (req, res, next) => {
       })
     })
   })
-}
+};
 
 function getFullAwb(awbs) {
   return new Promise((resolve, reject) => {
