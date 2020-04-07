948acf1887d7d7d965431f5904e3a97cd4deeb4f
Mayank Kumar <mayankblock09@gmail.com>
2020-04-07 09:57:32 +0530

    Fix - frontend: amount field validation issue on serviceType.

--- public/js/warehouse/service-type/create.js
+++ public/js/warehouse/service-type/create.js
@@ -7,6 +7,10 @@ $("#add-service-type-form").submit(function (event) {
   let formUrl = $(this).attr('action');
   var data = extractFormData(this);
 
+  // Converting the amount to integer before sending it to API
+  // and setting 0 as default if value is empty
+  data['amount'] = !data['amount'] ? 0 : parseInt(data['amount'], 10);
+
   $.ajax({
     url: formUrl,
     type: 'post',
