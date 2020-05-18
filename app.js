a1143ac5b0da71e24722c12722b143fd35d3416e
root <kkaushal@ongraph.com>
2020-05-18 19:34:26 +0530

    app.js Changes test5

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use(session({
     path: '/', 
     ephemeral: false, 
     httpOnly: true, 
-    secure: (process.env.NODE_ENV === "development" ? true : true),
+    secure: (process.env.NODE_ENV === "development" ? false : true),
   }
 }));
 
