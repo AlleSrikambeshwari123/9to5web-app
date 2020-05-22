c1679811dae10d6c76a789c0ffd0c10c93cc7759
root <kkaushal@ongraph.com>
2020-05-22 18:38:34 +0530

    Datatable Laststatux Text alert error

--- public/js/warehouse/awb/edit.js
+++ public/js/warehouse/awb/edit.js
@@ -446,7 +446,7 @@ $(function () {
           pkg.dimensions,
           Number(pkg.weight).toFixed(2) + ` ${pkg.packageCalculation || 'kg'}`,
           `${calculatePackageVolumetricWeight(pkg).toFixed(2)} lbs`,
-          pkg.lastStatusText,
+          pkg.lastStatusText ? pkg.lastStatusText : '',
           [
             `<a class="btn btn-link btn-primary btn-edit-pkg p-1" title="Edit" data-id="${pkg.id}" href="#add-package-popup"><i class="fa fa-pen"></i></a>`,
             `<a class="btn btn-link btn-danger btn-rm-pkg p-1" title="Delete" data-id="${pkg.id}" data-toggle='modal' data-target='#confirmPkgDel'><i class="fa fa-trash"></i></a>`,
