fbb12675923c32b2c09738c862e2931b6e9b80ae
root <kkaushal@ongraph.com>
2020-06-12 22:22:13 +0530

    Changes for Price Module

--- public/js/warehouse/price/list-all.js
+++ public/js/warehouse/price/list-all.js
@@ -11,7 +11,27 @@ let packageTable = $('.pricelabel-table').DataTable({
     selector: 'td:first-child input[type="checkbox"]',
   },
 });
-
+$('.checkPriceLabelExistPkg').map(function (i, dateElement) {
+  const id = dateElement.value;
+  checkPriceLabelExist(id)
+});
+function checkPriceLabelExist(id){
+  $.ajax({
+    url: '/warehouse/pricelabels/' + id,
+    type: 'get',
+    success: function (response) {
+      if(response.packageId){
+        return true
+      }else{
+        // $(`.ifNotPriceLabel${id}`).css({cursor:"not-allowed"});
+        // $(`.ifNotPriceLabel${id}`).attr("disabled",true);
+        // $(`.ifNotPriceLabel${id}`).removeAttr('data-toggle');
+        // $(`.ifNotPriceLabel${id}`).off("click");
+        $(`.ifNotPriceLabel${id}`).hide();
+        return false}
+    }
+  });
+}
 var pdfPath;
 $('#pricelabel-table').on('click', '.btn-print-pkg', function () {
   let id = $(this).data('id');
@@ -73,13 +93,16 @@ function pricelLabelCheck(response){
   $('#SedVal').text(response.Sed);
   $('#ExpressVal').text(response.Express);
   $('#TotalWet').val(TotalVat);
+  if(response.NoDocs >0) $('#NoDocs').prop('checked',true)
+  if(response.Insurance >0) $('#Insurance').prop('checked',true)
+  if(response.Sed >0) $('#Sed').prop('checked',true)
+  if(response.Express >0) $('#Express').prop('checked',true)
   pricelabelcommon(ServiceVat,NoDocsVal,InsuranceVal,SedVal,ExpressVal,totalinvoiceVal)
 }
 
 function packagePriceLabel(response){
   const pkg = response;
   let ServiceVat = 0;
-  let TotalVat = 0;
   let NoDocsVal = 0;
   let InsuranceVal = 0;
   let SedVal = 0;
@@ -152,7 +175,7 @@ function pricelabelcommon(ServiceVat,NoDocsVal,InsuranceVal,SedVal,ExpressVal,to
       Number($('#Storage').val())
     );
   }
-  $('#add-to-pricelabel-form').change(function () {
+  $('#add-to-pricelabel-form').on('keyup change', function () {
     tval();
     $('#ServiceVat').val(((tval() * 7.5) / 100).toFixed(2));
     $('#TotalWet').val((tval() + Number($('#ServiceVat').val())).toFixed(2));
@@ -160,8 +183,8 @@ function pricelabelcommon(ServiceVat,NoDocsVal,InsuranceVal,SedVal,ExpressVal,to
 }
 
 $('#pricelabel-table').on('click', '.btn-edit-pricelabel', function () {
-  let id = $(this).data('id');
-  var data = extractFormData($('#add-to-pricelabel-form'));
+  const id = $(this).data('id');
+  $('#setIdPriceLabel').val(id);
   $('#Brokerage').val(''),
   $('#CustomsProc').val(''),
   $('#CustomsVAT').val(''),
@@ -197,214 +220,54 @@ $('#pricelabel-table').on('click', '.btn-edit-pricelabel', function () {
     error:function(){
   }
   });
-  $(function () {
-    var addtoPriceLabel = $('#add-to-pricelabel-form');
-    addtoPriceLabel.submit(function (event) {
-      event.preventDefault();
-      data = {
-        Brokerage: $('#Brokerage').val() == "" ? 0 : $('#Brokerage').val(),
-        CustomsProc: $('#CustomsProc').val() == "" ? 0 : $('#CustomsProc').val(),
-        CustomsVAT: $('#CustomsVAT').val() == "" ? 0 : $('#CustomsVAT').val(),
-        Delivery: $('#Delivery').val() == "" ? 0 : $('#Delivery').val(),
-        Duty: $('#Duty').val() == "" ? 0 : $('#Duty').val(),
-        EnvLevy: $('#EnvLevy').val() == "" ? 0 : $('#EnvLevy').val(),
-        Freight: $('#Freight').val() == "" ? 0 : $('#Freight').val(),
-        Hazmat: $('#Hazmat').val() == "" ? 0 : $('#Hazmat').val(),
-        Pickup: $('#Pickup').val() == "" ? 0 : $('#Pickup').val(),
-        ServiceVat: $('#ServiceVat').val() == "" ? 0 : $('#ServiceVat').val(),
-        Storage: $('#Storage').val() == "" ? 0 : $('#Storage').val(),
-        NoDocs: $('#NoDocsVal').text() == "" ? 0 : $('#NoDocsVal').text(),
-        Insurance: $('#InsuranceVal').text() == "" ? 0 : $('#InsuranceVal').text(),
-        Sed: $('#SedVal').text() == "" ? 0 : $('#SedVal').text(),
-        Express: $('#ExpressVal').text() == "" ? 0 : $('#ExpressVal').text(),
-        TotalWet: $('#TotalWet').val() == "" ? 0 : $('#TotalWet').val(),
-      };
-      $.ajax({
-        url: '/warehouse/pricelabels/' + id,
-        type: 'post',
-        data: data,
-        success: function (response) {
-          $('#pricelabel-popup').modal('hide');
-          swal({
-            title: response.success ? 'Success' : 'Error',
-            type: response.success ? 'success' : 'error',
-            text: response.message,
-          });
-        },
-        error: function () {
-          swal({
-            title: 'Error',
-            type: 'error',
-            text: 'Unknown error',
-          });
-        },
+  
+});
+
+$('#UpdatePriceLabelPackage').on('click' , function (event) {
+  event.preventDefault();
+  var id = $('#setIdPriceLabel').val()
+  data = {
+    Brokerage: $('#Brokerage').val() == "" ? 0 : $('#Brokerage').val(),
+    CustomsProc: $('#CustomsProc').val() == "" ? 0 : $('#CustomsProc').val(),
+    CustomsVAT: $('#CustomsVAT').val() == "" ? 0 : $('#CustomsVAT').val(),
+    Delivery: $('#Delivery').val() == "" ? 0 : $('#Delivery').val(),
+    Duty: $('#Duty').val() == "" ? 0 : $('#Duty').val(),
+    EnvLevy: $('#EnvLevy').val() == "" ? 0 : $('#EnvLevy').val(),
+    Freight: $('#Freight').val() == "" ? 0 : $('#Freight').val(),
+    Hazmat: $('#Hazmat').val() == "" ? 0 : $('#Hazmat').val(),
+    Pickup: $('#Pickup').val() == "" ? 0 : $('#Pickup').val(),
+    ServiceVat: $('#ServiceVat').val() == "" ? 0 : $('#ServiceVat').val(),
+    Storage: $('#Storage').val() == "" ? 0 : $('#Storage').val(),
+    NoDocs: $('#NoDocsVal').text() == "" ? 0 : $('#NoDocsVal').text(),
+    Insurance: $('#InsuranceVal').text() == "" ? 0 : $('#InsuranceVal').text(),
+    Sed: $('#SedVal').text() == "" ? 0 : $('#SedVal').text(),
+    Express: $('#ExpressVal').text() == "" ? 0 : $('#ExpressVal').text(),
+    TotalWet: $('#TotalWet').val() == "" ? 0 : $('#TotalWet').val(),
+  };
+  $.ajax({
+    url: '/warehouse/pricelabels/' + id,
+    type: 'post',
+    data: data,
+    success: function (response) {
+      $('.close-del').trigger('click');
+      swal({
+        title: response.success ? 'Success' : 'Error',
+        type: response.success ? 'success' : 'error',
+        text: response.message,
       });
-    });
+      $(`.ifNotPriceLabel${id}`).show();
+    },
+    error: function () {
+      swal({
+        title: 'Error',
+        type: 'error',
+        text: 'Unknown error',
+      });
+    },
   });
 });
 
 $('.print-package').click(function () {
   $('.close-del').trigger('click');
   printJS(pdfPath);
-});
-
-$(function () {
-  let addToManifestModal = $('#add-to-manifest-modal');
-  let addToManifestForm = $('#add-to-manifest-form');
-  let compartmentIdSelect = addToManifestForm.find('[name="compartmentId"]');
-
-  let addToDeliveryModal = $('#add-to-delivery-modal');
-  let addToDeliveryForm = $('#add-to-delivery-form');
-
-  compartmentIdSelect.select2({
-    theme: 'bootstrap',
-    width: '100%',
-    placeholder: 'Select compartment',
-    dropdownParent: addToManifestModal,
-  });
-
-  $.ajax({
-    url: '/api/warehouse/get-manifests',
-    type: 'get',
-    dataType: 'json',
-    success(data) {
-      addToManifestForm
-        .find('[name="manifestId"]')
-        .select2({
-          theme: 'bootstrap',
-          width: '100%',
-          placeholder: 'Select manifest',
-          dropdownParent: addToManifestModal,
-          data: data.map((manifest) => ({
-            id: manifest.id,
-            text: manifest.title,
-            source: manifest,
-          })),
-        })
-        .on('select2:select', function (event) {
-          compartmentIdSelect.val(null).trigger('change');
-          compartmentIdSelect.find('option').remove();
-          compartmentIdSelect.prop('disabled', true);
-
-          let manifest = event.params.data && event.params.data.source;
-          const manifestPlanId = manifest && (manifest.planeId['_id'] || manifest.planeId);
-          if (manifestPlanId) {
-            loadCompartments(manifestPlanId);
-          }
-        });
-    },
-  });
-
-  $.ajax({
-    url: '/api/warehouse/get-deliverys',
-    type: 'get',
-    dataType: 'json',
-    success(data) {
-      addToDeliveryForm.find('[name="deliveryId"]').select2({
-        theme: 'bootstrap',
-        width: '100%',
-        placeholder: 'Select delivery',
-
-        data: data.map((delivery) => ({
-          id: delivery._id,
-          text: formatDate(delivery.delivery_date),
-        })),
-      });
-    },
-  });
-
-  function loadCompartments(planeId) {
-    compartmentIdSelect.prop('disabled', true);
-    $.ajax({
-      url: '/api/warehouse/get-compartments',
-      data: { planeId: planeId },
-      type: 'get',
-      dataType: 'json',
-      success(data) {
-        compartmentIdSelect.select2({
-          theme: 'bootstrap',
-          width: '100%',
-          placeholder: 'Select compartment',
-          dropdownParent: addToManifestModal,
-          data: data.map((compartment) => ({
-            id: compartment._id,
-            text: compartment.name,
-          })),
-        });
-
-        compartmentIdSelect.prop('disabled', false);
-      },
-    });
-  }
-
-  addToManifestForm.submit(function (event) {
-    addToManifestModal.modal('hide');
-    event.preventDefault();
-    var packageIds = packageTable
-      .rows({ selected: true })
-      .nodes()
-      .map((i) => $(i).data('record'))
-      .toArray()
-      .join(',');
-    addToManifestForm.find('[name="packageIds"]').val(packageIds);
-    var data = extractFormData(this);
-
-    $.ajax({
-      url: '/api/warehouse/add-packages-to-flight',
-      type: 'post',
-      data: data,
-      success: function (response) {
-        swal({
-          title: response.success ? 'Success' : 'Error',
-          type: response.success ? 'success' : 'error',
-          text: response.message,
-        });
-        const statusText = response.status;
-        const packageIds = (data['packageIds'] || '').split(',');
-
-        packageIds.forEach((packageId) => {
-          $(`tr[data-record="${packageId}"] > .lastStatusText_field`).text(statusText);
-        });
-      },
-      error: function () {
-        swal({
-          title: 'Error',
-          type: 'error',
-          text: 'Unknown error',
-        });
-      },
-    });
-  });
-
-  addToDeliveryForm.submit(function (event) {
-    addToDeliveryModal.modal('hide');
-    event.preventDefault();
-    var packageIds = packageTable
-      .rows({ selected: true })
-      .nodes()
-      .map((i) => $(i).data('record'))
-      .toArray()
-      .join(',');
-    addToDeliveryForm.find('[name="packageIds"]').val(packageIds);
-    var data = extractFormData(this);
-    $.ajax({
-      url: '/api/warehouse/add-packages-to-delivery',
-      type: 'post',
-      data: data,
-      success: function (response) {
-        swal({
-          title: response.success ? 'Success' : 'Error',
-          type: response.success ? 'success' : 'error',
-          text: response.message,
-        });
-      },
-      error: function () {
-        swal({
-          title: 'Error',
-          type: 'error',
-          text: 'Unknown error',
-        });
-      },
-    });
-  });
 });
