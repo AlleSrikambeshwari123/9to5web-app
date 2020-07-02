6c08ca726c0648f682f2ca6c577edcc1de193048
kkaushal-ongraph <kkaushal@ongraph.com>
2020-07-02 18:29:22 +0530

    no docs pkg added

--- routes/warehouse/awb.js
+++ routes/warehouse/awb.js
@@ -15,7 +15,9 @@ router.delete('/fll/awb/manage/:id/delete', middleware().checkSession, awbCtrl.d
 router.get('/fll/awb/manage/:awbId/print', middleware().checkSession, awbCtrl.generate_awb_pdf);
 
 router.get('/fll/awb/list', middleware().checkSession, awbCtrl.get_awb_list);
+
 router.get('/fll/awb/no-docs', middleware().checkSession, awbCtrl.get_awb_no_docs);
+
 router.post('/fll/awb/create-barcode', awbCtrl.add_bar_code);
 router.get('/fll/awb/refresh-barcode', awbCtrl.refresh_barcode);
 
