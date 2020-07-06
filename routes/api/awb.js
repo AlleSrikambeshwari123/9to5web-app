78c643709de68525d1ccbc98802c6944774ed74c
kumar <kkaushal@ongraph.com>
2020-07-06 14:51:58 +0530

    list awb

--- routes/api/awb.js
+++ routes/api/awb.js
@@ -29,7 +29,7 @@ router.get('/awb/:id',async(req, res, next) => {
     }
 });
 
-//list awb without invoice
+//list awb without invoices
 router.get('/awb-without-invoice/:id',async(req, res, next) => {
     try{   
         const customerId = req.params.id;     
