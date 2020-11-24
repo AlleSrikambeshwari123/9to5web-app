6f0c3b0e2460dec36d8833b29eba7670fea4e58d
gaurav <js.developer@ongraph.com>
2020-11-24 13:40:55 +0530

    test print api

--- routes/util.js
+++ routes/util.js
@@ -52,8 +52,8 @@ router.post('/upload', function (req, res) {
 
 router.get('/pdf/:pdfname', (req, res, next) => {
   console.log("checl0",global.uploadRoot + '/' + req.params.pdfname)
-  res.download(global.uploadRoot + '/' + req.params.pdfname,req.params.pdfname,(err)=>{
-    console.log("err",err)
+  res.download(global.uploadRoot + '/' + req.params.pdfname,function (err,result){
+    console.log("err",err,result)
     if(err)
       return err
   });
