c13745b67649909688b12a5f0e5b250ee35e6c31
Allen McIntosh II <allen.mcintoshii@gmail.com>
2021-05-14 14:59:21 -0400

    Correct Airport.Service.all(); remove checkSession()

--- middleware.js
+++ middleware.js
@@ -12,30 +12,30 @@ module.exports = function (allowedRoles) {
     }
     return {
         checkSession: async function (req, res, next) {
-            var token = req.session.token;
-            if (token) {
-                utils.verifyToken(token).then(function (user) {
-                    res.user = user;
-                    // Temporary disable the printer
-                    // res.printer = services.printService.getUserPrinter(user.username);
-                    var navMode = handleNavigation(user);
-                    res.navigationMode = navMode;
-                    req['userId'] = user['_id'];
-                    req['username'] = user['username'];
-                    req['user'] = user;
+            // var token = req.session.token;
+            // if (token) {
+            //     utils.verifyToken(token).then(function (user) {
+            //         res.user = user;
+            //         // Temporary disable the printer
+            //         // res.printer = services.printService.getUserPrinter(user.username);
+            //         var navMode = handleNavigation(user);
+            //         res.navigationMode = navMode;
+            //         req['userId'] = user['_id'];
+            //         req['username'] = user['username'];
+            //         req['user'] = user;
 
-                    if (Array.isArray(allowedRoles))
-                        if (allowedRoles.indexOf(user.role) < 0) {
-                            res.status(401).render('pages/401', { user: user });
-                        }
-                    next();
-                }, function () {
-                    res.redirect('/')
-                    // res.status(401).render('pages/401', { title: 'Express', failed: '0' });
-                });
-            }
-            else
-                res.redirect('/')
+            //         if (Array.isArray(allowedRoles))
+            //             if (allowedRoles.indexOf(user.role) < 0) {
+            //                 res.status(401).render('pages/401', { user: user });
+            //             }
+            //         next();
+            //     }, function () {
+            //         res.redirect('/')
+            //         // res.status(401).render('pages/401', { title: 'Express', failed: '0' });
+            //     });
+            // }
+            // else
+            //     res.redirect('/')
         },
     }
 };
