8fd3894ef09beb71ccf8e6a51f8b0b9f4e1e145c
kkaushal <kkaushal@ongraph.com>
2021-07-22 17:03:12 +0530

    login fix

--- routes/customer/login.js
+++ routes/customer/login.js
@@ -11,32 +11,32 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, global.uploadRoot+'/')
+    cb(null, global.uploadRoot + '/')
   },
   filename: function (req, file, cb) {
-      const fext = (file.originalname).split('.').pop();
-    cb(null, uniqid() + '_' + moment().utc().unix() + "."+fext) //Appending extension
+    const fext = (file.originalname).split('.').pop();
+    cb(null, uniqid() + '_' + moment().utc().unix() + "." + fext) //Appending extension
   }
 });
-var upload = multer({ storage: storage});
+var upload = multer({ storage: storage });
 
 
 router.get('/login', function (req, res, next) {
-  
+
   if (req.session.token)
     res.redirect('/dashboard');
-  else  
+  else
     res.render('login')
-});    
+});
 
 router.post('/login', (req, res, next) => {
-  services.customerService.login(req.body.email, req.body.password).then(loginResult => {
-    if(loginResult.success || loginResult.authenticated){
+  services.customerService.login(req.body.email.replace("%40", "@"), req.body.password).then(loginResult => {
+    if (loginResult.success || loginResult.authenticated) {
       req.session.token = loginResult.token;
       req.session.customerId = loginResult.user._id
-  const webUrl = req.protocol + '://' + req.get('host'); 
-  res.send({success : true ,role : loginResult,url : webUrl + '/customer/report/package-report'});
-    }else{
+      const webUrl = req.protocol + '://' + req.get('host');
+      res.send({ success: true, role: loginResult, url: webUrl + '/customer/report/package-report' });
+    } else {
       services.customerChildService.login(req.body.email, req.body.password).then(loginResultchild => {
         return res.send(loginResultchild)
       })
@@ -44,47 +44,47 @@ router.post('/login', (req, res, next) => {
   })
 });
 
-router.get('/dashboard', middleware().checkSession,customerCtrl.get_customer_awb_list);
+router.get('/dashboard', middleware().checkSession, customerCtrl.get_customer_awb_list);
 
 
 router.get('/change-pass', middleware().checkSession, function (req, res, next) {
-	res.render('pages/customer/account/change-pass', {
-		page: '/account' + req.url,
-		title: "Change Password",
-		user: res.user,
-	});
+  res.render('pages/customer/account/change-pass', {
+    page: '/account' + req.url,
+    title: "Change Password",
+    user: res.user,
+  });
 });
 
 router.post('/change-pass', middleware().checkSession, function (req, res, next) {
-  services.customerService.changePassword({email : res.user.email,password : req.body.password, oldPassword : req.body.oldpass}).then(result => {
-		res.send(result)
+  services.customerService.changePassword({ email: res.user.email, password: req.body.password, oldPassword: req.body.oldpass }).then(result => {
+    res.send(result)
   })
 });
 
-router.get('/forgot-password', function (req, res, next) {  
+router.get('/forgot-password', function (req, res, next) {
   res.render('customer_forgot_password');
 });
 
-router.post('/request-pwd-reset',async function(req,res,next){
+router.post('/request-pwd-reset', async function (req, res, next) {
   const webUrl = req.protocol + '://' + req.get('host');
   const result = await services.customerService.requestResetPassword(req.body.email, webUrl);
   res.send(result);
 });
 
-router.get('/awb', middleware().checkSession,customerCtrl.get_customer_awbs);
+router.get('/awb', middleware().checkSession, customerCtrl.get_customer_awbs);
 
-router.get('/awb/:id/preview', middleware().checkSession,customerCtrl.preview_awb);
+router.get('/awb/:id/preview', middleware().checkSession, customerCtrl.preview_awb);
 
-router.get('/billing', middleware().checkSession,customerCtrl.billing);
+router.get('/billing', middleware().checkSession, customerCtrl.billing);
 
-router.get("/upload/invoices",middleware().checkSession,customerCtrl.upload_invoices);
+router.get("/upload/invoices", middleware().checkSession, customerCtrl.upload_invoices);
 
-router.post("/upload/invoice",middleware().checkSession,upload.single('invoice'),customerCtrl.upload_invoice);
+router.post("/upload/invoice", middleware().checkSession, upload.single('invoice'), customerCtrl.upload_invoice);
 
-router.get("/account/profile",middleware().checkSession,customerCtrl.profile);
+router.get("/account/profile", middleware().checkSession, customerCtrl.profile);
 
-router.post("/update/profile",middleware().checkSession,customerCtrl.updateProfile);
+router.post("/update/profile", middleware().checkSession, customerCtrl.updateProfile);
 
 
-router.get("/report/package-report",middleware().checkSession,customerCtrl.packageReport);
+router.get("/report/package-report", middleware().checkSession, customerCtrl.packageReport);
 module.exports = router;
