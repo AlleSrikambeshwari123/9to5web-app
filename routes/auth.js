57964babcba4ced0d0985eb9d084e1ec0d584147
kkaushal <kkaushal@ongraph.com>
2021-08-20 13:04:48 +0530

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -73,8 +73,8 @@ router.get('/reset-password/customer/success', function(req,res, next){
 //   result.postbox = false;
 //   res.render('password-set-new-customer',result);     
 // });
-let resetmail = process.env.RESETMAIL_URL.replace('/customer','');
-resetmail = resetmail.replace('\n','') ;
+// let resetmail = process.env.RESETMAIL_URL.replace('/customer','');
+let resetmail = resetmail.replace('\n','') ;
 router.get(`${resetmail}`,async function(req, res, next){
   const result = await services.customerService.getUserByResetPasswordToken(req.params.id);
   result.reset_link = '/reset-password/customer/' + req.params.id;
